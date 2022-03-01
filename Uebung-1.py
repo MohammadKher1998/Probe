735797b9d412f6217bb04f8649649c6e9006dbae
MohammadKher1998 <mohamedkhairshalabe@gmail.com>
2022-03-01 16:12:29 +0100

    new probe

---
+++ Uebung-1.py
@@ -0,0 +1,13 @@
+# age = 22
+# print ("ich bin " + str(age) +  "jahre alt")
+#students = ["ahmad" , "sanir", "moritz"] 
+#print("ahmad" in students)
+# students.append("mohammad")
+# print (" & ".join(students))
+
+result = 5 > 7
+print (result)
+#if result:
+    #print("6 ist kleiner als 5")
+#x = 20 
+#print ("majd " + str(x))
